a08c759fd341086ee2a582f27eb523957039e23c
Théo Arrouye <5017270+tarrouye@users.noreply.github.com>
2019-04-16 20:02:07 -0700

    Update FakeGameCenter.lua

--- Clumsy Surfing/ClumsySurfing.codea/FakeGameCenter.lua
+++ Clumsy Surfing/ClumsySurfing.codea/FakeGameCenter.lua
@@ -5,5 +5,9 @@ gamecenter = {
   
   submitScore = function(num, name)
     print("Submitted GameCenter score for ID: " .. name .. " : " .. num)
+  end,
+  
+  showLeaderboards = function(name)
+    print("Would be showing GameCenter Leaderboard ID: " .. name)
   end
 }
