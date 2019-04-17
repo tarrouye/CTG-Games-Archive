c13469aeca63ab094972be6272b681f353bcabff
Théo Arrouye <5017270+tarrouye@users.noreply.github.com>
2019-04-16 20:26:58 -0700

    Create FakeIAP.lua
    Stump functions to replace the In-App Purchases Add-On used in the App Store version

---
+++ Scramblers/Scramblers.codea/FakeIAP.lua
@@ -0,0 +1,15 @@
+registerItem = function(id)
+  print("Registered shop item: " .. id)
+end
+
+initStore = function() 
+  print("Initialized store")
+end
+
+purchaseItem = function(id)
+  print("Purchased item: " .. id)
+end
+
+restorePurchases = function()
+  print("Restored purchases")
+end
